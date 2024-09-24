57833d21e5d5a238e5376084d65e649f2748ead1
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2024-09-25 01:38:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import search from './search'
 
 function App() {
   const handleClick = () => {
-    const randSearch = search[Math.floor(Math.random() * (6000 - 4000 + 1) + 4000 )]
+    const randSearch = search[Math.floor(Math.random() * (10000 - 1 + 1) + 1 )]
     // 74998
     
     window.location = `https://yewtu.be/search?q=${randSearch}`
